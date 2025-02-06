7f70a1513745881c8444c15cdf87c24ea42c6c67
vai <vai.sriv@icloud.com>
2025-02-06 16:36:16 -0500

    update init.lua for arrow.yazi

--- arrow.yazi/init.lua
+++ arrow.yazi/init.lua
@@ -1,7 +1,7 @@
 --- @sync entry
 
 return {
-	entry = function(_, args)
+	entry = function(_, job.args)
 		local current = cx.active.current
 		local new = (current.cursor + args[1]) % #current.files
 		ya.manager_emit("arrow", { new - current.cursor })
